b83d7779fb932b056e6435b0efbb4526fd21dab5
grz17 <resendizgabriel19@gmail.com>
2021-07-25 23:46:55 -0500

    creacion de la carpeta servicos al igual manera que codificacion de codigo  de los servicios de la aplicacion , Modificacion de las  vistas  de inio y registro de usuario tanto de scss y html  modicacion del index global

--- src/app/registrarse/registrarse.page.ts
+++ src/app/registrarse/registrarse.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ApiService} from '../Servicios/api.service';
 @Component({
   selector: 'app-registrarse',
   templateUrl: './registrarse.page.html',
@@ -7,9 +7,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistrarsePage implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
+ mensajes:any[]=[];
+ apellido: any="";
+ nombre: string="";
+ username:string="";
+ correo: any="";
+ contra: any="";
+ rpassword: string="";
+  constructor( private servicioservice: ApiService) { }
+  ngOnInit(): void {
+    this.servicioservice.getnuevo(this.apellido,this.nombre,this.username,this.correo,this.contra,this.rpassword).subscribe( (post:any)=> {
+     console.log(post);
+     this.mensajes=post.Estatus;
+     if(post.estado==false&&post.detalle[0]=="Correo o password incorrectos")
+     {
+      alert("Usuario O Contraseña Erroneo");
+     }
+     if(post.estado==true){
+        document.location.href="iniciosesion";
+       
+     }
+    });
+    
   }
 
 }
